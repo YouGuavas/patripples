05eb2d4f885e913848763cef62bf4743602c1f7f
YouGuavas <patrickyambrick@gmail.com>
2025-04-23 20:16:40 -0400

    Adjust styles

--- app/components/ByLine.tsx
+++ app/components/ByLine.tsx
@@ -3,7 +3,7 @@ type propsType = {
 };
 export default function ByLine(props: propsType) {
 	return (
-		<div className={`left full left-align`}>
+		<div className={`left width-full left-align`}>
 			<p className={`style-1 lowercase left-align`}>Published: {props.date}</p>
 		</div>
 	);
