7717ae7be84b7da57d3c726b3d297c07e2e239e4
YouGuavas <patrickyambrick@gmail.com>
2025-04-27 22:29:47 -0400

    Update contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -14,10 +14,10 @@ export default function Home() {
 		<SpaceProviderWrapper>
 			<main className={`main center`}>
 				<h1 className={`lowercase style-1 thin`}>Want to ask me something?</h1>
-				<p className={`style-1 uppercase`}>
+				<p className={`style-1 lowercase`}>
 					Email:{' '}
 					<Link
-						className={`no-deco style-1 uppercase`}
+						className={`no-deco style-1 spaced lowercase`}
 						href="mailto:pat@patripples.com"
 					>
 						pat@patripples.com
@@ -33,12 +33,12 @@ export default function Home() {
 				<h2 className="lowercase style-1 thin">
 					Want to know about your privacy?
 				</h2>
-				<p className={`style-1`}>
+				<p className={`paragraph`}>
 					I use Vercel&apos;s web analytics to understand how this site is used
 					and to improve your experience. This data is anonymous and does not
 					include any personal or sensitive information.
 				</p>
-				<p className={`style-1`}>
+				<p className={`paragraph`}>
 					I store email addresses and first names - as provided in the email
 					form on the home page - in a database for use in personal mailing
 					lists. This allows me to keep my site free and clean, with minimal
