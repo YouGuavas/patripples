38c3b36ac4d1af66a88a62f8957809548e2ed0f7
YouGuavas <patrickyambrick@gmail.com>
2025-04-27 20:20:46 -0400

    Update filters

--- app/see/routes.ts
+++ app/see/routes.ts
@@ -2,12 +2,12 @@ const routes = [
 	{
 		name: 'a walk in the park',
 		path: '/see/a_walk_in_the_park',
-		tags: ['swamp arts', 'crow alchemy', 'x'],
+		tags: ['swamp science', 'crow alchemy', 'x'],
 	},
 	{
 		name: 'letting things breathe',
 		path: '/see/letting_things_breathe',
-		tags: ['swamp arts', 'crow alchemy', 'x'],
+		tags: ['swamp science', 'crow alchemy', 'x'],
 	},
 ];
 
