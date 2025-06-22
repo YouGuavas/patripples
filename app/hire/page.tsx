75e78b61e7f410be8d4c929000e61bf70f896434
YouGuavas <patrickyambrick@gmail.com>
2025-06-21 23:10:11 -0400

    Add hire page

---
+++ app/hire/page.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import type { Metadata } from 'next';
+import Link from 'next/link';
+
+import styles from '../page.module.css';
+
+export const metadata: Metadata = {
+	title: 'Patripples | Contact',
+	description: 'You can contact Pat by emailing pat@patripples.com',
+};
+export default function Home() {
+	return (
+		<main className={`main center`}>
+			<h1 className="lowercase style-1 thin">
+				Want Pat to make you a website?
+			</h1>
+			<p className={`style-1`}>
+				Pay me on{' '}
+				<Link className={`no-deco style-1`} href="https://cash.app/$pyambrick">
+					Cashapp
+				</Link>
+				.
+			</p>
+
+			<p className={`style-1`}>
+				Hire me on{' '}
+				<Link
+					className={`no-deco style-1`}
+					href="https://www.fiverr.com/s/YRlxwja/"
+				>
+					Fiverr
+				</Link>
+				.
+			</p>
+		</main>
+	);
+}
--- app/routes.ts
+++ app/routes.ts
@@ -4,6 +4,8 @@ import eye from '../public/universal/eye.svg';
 import book from '../public/universal/book.svg';
 import count from '../public/universal/count.svg';
 import pat from '../public/universal/pat.svg';
+import dollar from '../public/universal/dollar.svg';
+
 import ask from '../public/universal/ask.svg';
 
 const routes = [
@@ -13,6 +15,7 @@ const routes = [
 	{ name: 'Read', path: '/read', image: book },
 	{ name: 'Count', path: '/count', image: count },
 	{ name: 'Character', path: '/character', image: pat },
+	{ name: 'Hire', path: '/hire', image: dollar },
 
 	{ name: 'Ask', path: '/contact', image: ask },
 ];
