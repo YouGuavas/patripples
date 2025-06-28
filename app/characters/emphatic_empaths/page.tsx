9a8d82cc68928ededef68ea3a0ecdbde38ce349e
YouGuavas <patrickyambrick@gmail.com>
2025-06-28 17:51:11 -0400

    Update empaths page

--- app/characters/emphatic_empaths/page.tsx
+++ app/characters/emphatic_empaths/page.tsx
@@ -1,5 +1,11 @@
 import { characterType } from '../characters';
 import { complete } from '../characters';
+import Image from 'next/image';
+import the_banana from '../../../public/characters/the_banana.svg';
+import the_girls from '../../../public/characters/the_girls.svg';
+import the_bloomkeeper from '../../../public/characters/the_bloomkeeper.svg';
+import alloy_alchemist from '../../../public/characters/alloy_alchemist.svg';
+import the_foretold from '../../../public/characters/the_foretold.svg';
 import CharacterSheet from '../../components/CharacterSheet';
 import type { Metadata } from 'next';
 
@@ -28,24 +34,50 @@ export default function Character() {
 	return (
 		<main className={`main center height-full`}>
 			<h1 className={`style-1 thin lowercase`}>
-				The Emphatic Empaths Character Card.
+				The Emphatic Empaths Character Page.
 			</h1>
 
 			<section className={`flex gap-large center`}>
-				<CharacterSheet
-					phase={phase}
-					title={title}
-					archetype={archetype}
-					stats={stats}
-					photo={photo}
-					portrait={portrait}
-					mythic={mythic}
-					standard_bio={standard_bio}
-					mythic_bio={mythic_bio}
-					href={href}
-					alt={alt}
-					tier={tier}
-				/>
+				<div className="jumbo-container bg-6">
+					<Image src={photo} alt={alt} fill />
+				</div>
+				{standard_bio.map((paragraph: string, index) => {
+					return (
+						<p key={index} className={`width-full left-align paragraph`}>
+							{paragraph}
+						</p>
+					);
+				})}
+				<div className="jumbo-container bg-6">
+					<Image src={portrait} alt={alt} fill />
+				</div>
+				{mythic_bio.map((paragraph: string, index) => {
+					return (
+						<p key={index} className={`width-full left-align paragraph`}>
+							{paragraph}
+						</p>
+					);
+				})}
+				<div className="jumbo-container bg-6">
+					<Image src={mythic} alt={alt} fill />
+				</div>
+				<div className="flex gap-small center">
+					<div className="image-container bg-6">
+						<Image src={the_girls.src} alt={alt} fill />
+					</div>
+					<div className="image-container bg-6">
+						<Image src={the_bloomkeeper.src} alt={alt} fill />
+					</div>
+					<div className="image-container bg-6">
+						<Image src={alloy_alchemist.src} alt={alt} fill />
+					</div>
+					<div className="image-container bg-6">
+						<Image src={the_foretold.src} alt={alt} fill />
+					</div>
+					<div className="image-container bg-6">
+						<Image src={the_banana.src} alt={alt} fill />
+					</div>
+				</div>
 			</section>
 		</main>
 	);
