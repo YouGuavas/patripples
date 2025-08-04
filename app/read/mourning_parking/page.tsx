e3aa27c6473b97d48a75eb1fbc141e08afb77f4e
unknown <patrickyambrick@gmail.com>
2025-08-04 05:41:14 -0700

    Finish story

--- app/read/mourning_parking/page.tsx
+++ app/read/mourning_parking/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
             <h2
                 className={`lowercase style-1 thin spaced left width-full block left-align`}
             >
-                a lot scene.
+                story.
             </h2>
             <p className={`paragraph width-full block left-align`}>
                 &quot;Woo!&quot;
@@ -36,16 +36,78 @@ export default function Home() {
             <p className={`paragraph width-full block left-align`}>
                 But she&apos;d never been there before four, before.
             </p>
-            <Image
-                id="crow"
-                alt={`A crow through the trees.`}
-                height={333}
-                width={250}
-                src={crow.src}
-            />
-            
+            <p className={`paragraph width-full block left-align`}>
+This morning was made for her!            </p>
+            <p className={`paragraph width-full block left-align`}>
+She was going to make so many donuts. She was going to get out early on this perfect Saturday. Even if her kids were too busy to visit again, she was going to have energy for Pepper, the petite pupper, and she was going to read a bit of her new book.            </p>
+
+                    <p className={`paragraph width-full block left-align`}>
+She&apos;d gotten a good night&apos;s rest, and even woke for her alarm on the scecond try - another personal best! On top of all of that, today&aspos;s weather was going to be flawless! She was ecstatic!
+            </p>  ]
+            <hr />
+            <p className={`paragraph width-full block left-align`}>
+                &quot;Goddamn it!&quot; shouted her equally-early coworker, less enthused.
+            </p>
+            <p className={`paragraph width-full block left-align`}>
+                &quot;What&apos;s the matter?&quot; she asked, her voice carrying slightly less of that charming chime she was known for.
+            </p>  
+            <p className={`paragraph width-full block left-align`}>
+                &quot;Boss gave me the key, but no code. We can&apos;t get in!&quot; he replied as he jangled toward his truck.
+            </p> 
+            <p className={`paragraph width-full block left-align`}>
+                &quot;Oh, my! That&apos;s no good!&quot; she chirped. &quot;Can you call someone?&quot;
+            </p>  
+            <p className={`paragraph width-full block left-align`}>
+                &quot;It&apos;s no use. Aside from Boss, Joss and Ross are the only ones who know the code. Boss took Saturday off, Ross has been MIA for weeks, and Joss refuses work contact outside her shift. She&apos;s not in until six. You&apos;ve been here fifteen years, you know it&apos;s always like this. I&apos;m going to get some sleep. You&apos;d do well to do the same.&quot;
+            </p>   
+            <p className={`paragraph width-full block left-align`}>
+                &quot;Oh, that&apos;s okay... I think I&apos;ll sta-&quot;
+            </p>
+            <p className={`paragraph width-full block left-align`}>
+Her coworker starts his truck and pulls away.            </p>
+<p className={`paragraph width-full block left-align`}>
+                &quot;-just in case.&quot; and she turns and walks back to her truck, head hanging.
+            </p>  
+            <hr />
+            <p className={`paragraph width-full block left-align`}>
+                He had been right. She wasn&apos;t young. She had been through it. Seen how things were. She wasn&apos;t dumb, either - just courageously kind, if only through spite. She knew that this wasn&apos;t her forever home, and had never intended to be here this long.
+            </p>
+            <p className={`paragraph width-full block left-align`}>
+                But she hadn&apos;t only seen herself and others around her trading their freedom and their dreams for survival and other men&apos;s schemes. She had seen far too many could-have-been-good days soured like spoiled milk following small spills. She had also learned to see a little light peeping at the edges of this seemingly endless in-between.
+            </p>
+            <p className={`paragraph width-full block left-align`}>
+                Somehow, she had managed to find hope in that hometown grocery community.
+            </p>
+            <hr />
+            <p className={`paragraph width-full block left-align`}>
+                And here came her other coworker, anyway. Surely, she would save the day. The woman waved as she hopped down out of her truck and walked up to the green Chevy Cruze that had pulled into the lot nearby.
+            </p>
+            <p className={`paragraph width-full block left-align`}>
+                The driver rolled down the window.
+            </p>
+            <p className={`paragraph width-full block left-align`}>
+                Ahh! A man. Is he ho-
+            </p>
+<p className={`paragraph width-full block left-align`}>
+                &quot;-I&apos;m a wanderer. Well, I&apos;m from here, but I&apos;m in a wandering phase. I&apos;m not sleeping here or anything.&quot; he presciently <over-explained className=""></over-explained>
+            </p>
+            <p className={`paragraph width-full block left-align`}>
+&quot;Oh, I- I don&apos;t care about that. I thought you were my coworker.&quot;            </p>
+<p className={`paragraph width-full block left-align`}>
+                The wanderer smiled softly, and the woman continued, loosely explaining what had just unfurled.
+            </p>
+            <hr />
+            <p className={`paragraph width-full block left-align`}>
+                Following a brief conversation about the unfairness of her predicament, the wanderer gave the woman such a kind compliment about her energy that, although she could no longer recall his words, she did remember feeling validated and seen in her defiant kindness all day.
+            </p>
             </section>
-            
+            <section>
+            <h2
+                className={`lowercase style-1 thin spaced left width-full block left-align`}
+            >
+                essay coming later today.
+            </h2>
+            </section>
         </main>
     );
 }
