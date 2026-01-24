2901e36e002ff533e5b2a376b066b2eb9e92d41f
YouGuavas <patrickyambrick@gmail.com>
2026-01-24 16:20:02 -0500

    SEO

--- app/hire/services.ts
+++ app/hire/services.ts
@@ -29,7 +29,7 @@ export const services = [
 	{
 		title: 'Websites',
 		description: [
-			'Patrick builds websites for small businesses, homesteaders, creative trend-setters, and other go-getters.',
+			'A web developer based in Flint, Michigan, Patrick builds websites for small businesses, homesteaders, creative trend-setters, and other go-getters.',
 		],
 		services: [
 			{
