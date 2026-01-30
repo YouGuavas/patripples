e3ff6f70516e52528b33d6819f579e74d28e9f9b
YouGuavas <patrickyambrick@gmail.com>
2026-01-30 12:16:17 -0500

    Update byline styles

--- app/components/ByLine.tsx
+++ app/components/ByLine.tsx
@@ -1,15 +1,21 @@
 type propsType = {
 	author: String;
 	date: String;
+	edited: String;
 };
 export default function ByLine(props: propsType) {
 	return (
-		<div className={`left width-full left-align flex column`}>
-			<p className={`style-1 lowercase left-align`}>
+		<div className={`left width-full left-align flex column gap-small`}>
+			<p className={`margin-none style-1 lowercase left-align`}>
 				By: {props.author ? props.author : 'Patrick Yambrick'}
 			</p>
 
-			<p className={`style-1 lowercase left-align`}>Published: {props.date}</p>
+			<p className={`margin-none style-1 lowercase left-align`}>
+				Published: {props.date}
+			</p>
+			<p className={`margin-none style-1 lowercase left-align`}>
+				Last Edit: {props.edited ? props.edited : props.date}
+			</p>
 		</div>
 	);
 }
