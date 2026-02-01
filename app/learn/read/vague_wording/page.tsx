176b5817e8685ca1807a7e889c6f2cf3df25d7a8
YouGuavas <patrickyambrick@gmail.com>
2026-02-01 18:42:54 -0500

    Write vague wording post

--- app/learn/read/vague_wording/page.tsx
+++ app/learn/read/vague_wording/page.tsx
@@ -4,10 +4,13 @@ import raditz_goku from '@/public/memes/raditz_goku.jpeg';
 
 import type { Metadata } from 'next';
 import ByLine from '@/app/components/ByLine';
+import Footer from '@/app/components/blog/Footer';
 
 export const metadata: Metadata = {
-	title: "Patripples | 'What Is Patripples' by Patrick Yambrick",
-	description: '. Written by Patrick Yambrick.',
+	title:
+		"Patripples | 'Patrick\'s Pitfalls - Vague Wording' by Patrick Yambrick",
+	description:
+		"Patrick's Pitfalls is a series dedicated to sharing painfully earned wisdom in a digestible format. This one is about vague wording, how it crushes web traffic, and how to fix it. Written by Patrick Yambrick.",
 };
 export default function Home() {
 	return (
@@ -15,12 +18,12 @@ export default function Home() {
 			<h1
 				className={`lowercase style-1 thin spaced left width-full block left-align`}
 			>
-				How Patrick Builds A Website
+				Patrick&apos;s Pitfalls - Vague Wording
 			</h1>
 			<ByLine
 				author={`Patrick Yambrick`}
-				date={'Friday, Jan 30, 2026'}
-				edited={'Friday, Jan 30, 2026'}
+				date={'Sunday, Feb 1, 2026'}
+				edited={'Sunday, Feb 1, 2026'}
 			/>
 
 			<br />
@@ -31,11 +34,11 @@ export default function Home() {
 					What Are Patrick&apos;s Pitfalls?
 				</h2>
 				<p className={`paragraph width-full block left-align`}>
-					I have always been a fan of the saying, "Smart people learn from their
-					mistakes. Wise people learn from the mistakes of others." In that
-					context, I offer you Patrick&apos;s Pitfalls: wisdom painfully earned
-					and mistakes carelessly made, so that you don&apos;t waste your time
-					doing the same.
+					I have always been a fan of the saying, &quot;Smart people learn from
+					their mistakes. Wise people learn from the mistakes of others.&quot;
+					In that context, I offer you Patrick&apos;s Pitfalls: wisdom painfully
+					earned and mistakes carelessly made, so that you don&apos;t waste your
+					time doing the same.
 				</p>
 				<p className={`paragraph width-full block left-align`}>
 					As someone who likes to try their hand at everything, and who insists
@@ -54,11 +57,11 @@ export default function Home() {
 					Today&apos;s Pitfall: Vague Wording.
 				</h2>
 				<p className={`paragraph width-full block left-align`}>
-					"Welcome to my class on tensor analysis. It is about tensors, etc.
-					Hope that helps." We have all done something like this. If you are
-					looking at this page, that is because you are an intelligent person.
-					You are no doubt talented in your domain. When you think about that
-					niche thing within your domain, you mentally conjure its shape so
+					&quot;Welcome to my class on tensor analysis. It is about tensors,
+					etc. Hope that helps.&quot; We have all done something like this. If
+					you are looking at this page, that is because you are an intelligent
+					person. You are no doubt talented in your domain. When you think about
+					that niche thing within your domain, you mentally conjure its shape so
 					easily that you sometimes forget how hard it was for you to get where
 					you are. How clear, meticulous, and deliberate you had to be while you
 					were learning.
@@ -83,7 +86,7 @@ export default function Home() {
 				/>
 				<p className={`paragraph width-full block left-align`}>
 					If you&apos;re not, it&apos;s like that kid who used to tease you in
-					elementary school who you haven't seen or heard from in decades.
+					elementary school who you haven&apos;t seen or heard from in decades.
 				</p>
 				<p className={`paragraph width-full block left-align`}>
 					That&apos;s what you&apos;ve done with your craft. You should be
@@ -98,7 +101,8 @@ export default function Home() {
 					Why Is Vague Wording Bad?
 				</h2>
 				<p className={`paragraph width-full block left-align`}>
-					An excellent question. Vague wording kills ads in a number of ways.
+					An excellent question. Vague wording crushes ad traffic and
+					followthrough in a number of ways.
 				</p>
 				<h3
 					className={`lowercase style-1 thin spaced left width-full block left-align`}
@@ -113,41 +117,149 @@ export default function Home() {
 				<p className={`paragraph width-full block left-align`}>
 					We&apos;ve got to make the algorithm happy.
 				</p>
-				<p className={`paragraph width-full block left-align`}>
-					How do we appease AI?
-				</p>
+
 				<ul
-					className={`no-deco pad-none centered flex column gap-small left-align width-full`}
+					className={`no-deco pad-none flex column gap-small left-align width-half`}
 				>
+					<h4
+						className={`lowercase style-1 thin spaced left width-full block left-align`}
+					>
+						How do we keep AI happy?
+					</h4>
 					<li>
 						<strong>Clear Wording</strong> - AI and the algorithm like it when
-						we are clear with our words. Clear wording helps any filtration
-						system, digital or biological, to efficiently sift through our
-						thought and emerge with something of meaning. This is good for small
-						business owners and creatives alike.
+						we are clear with our words. Clear wording helps filtration systems
+						- digital and biological - to efficiently sift through our written
+						thought and emerge with meaning.
 					</li>
 					<li>
-						<strong>Focused Content</strong> - Where clear wording allows
-						visitors, AI, and search engines to efficiently sift through the
-						ideas within our words, delivering focused content helps those same
-						observers to efficiently determine the directionality of our
-						thoughts.
+						<strong>Focused Content</strong> - Where clear wording allows AI and
+						search engines to efficiently sift through the ideas within our
+						words, delivering focused content helps them to determine the
+						directionality of our thoughts.
 					</li>
 					<li>
 						<strong>Appropriate Tone</strong> - If wording helps with filtration
 						efficiency, and content focus helps with determining thought
-						directionality, then tone projects intent.
+						directionality, then tone projects intent. Some AI can detect{' '}
+						<Link
+							className={`no-deco lowercase style-1 thin spaced color-2 rounded darken`}
+							href="https://builtin.com/artificial-intelligence/grammarly-tone-detector"
+						>
+							40 different tones
+						</Link>
+						. When AI or the algorithm can quickly and accurately determine your
+						tone, they can then make inferences about your intent which help
+						them to better index your content.
 					</li>
 				</ul>
+				<p className={`paragraph width-full block left-align`}>
+					Adjustments to any of these three go a long way, and since they are
+					interconnected, adjustments tend to compound. When we make a change to
+					tone, say, not only does it often implicitly also change our clarity,
+					but the change in tone impacts and amplifies any deliberate changes in
+					clarity or focus which we choose to enact.
+				</p>
+				<p className={`paragraph width-full block left-align`}>
+					We can keep AI and the algorithm happy by being clear, focused,
+					concise, and direct. Before I share my next ad or blog post, I am
+					going to take the extra time to scan it for simplifications,
+					clarifications, and anything in the tone which implies grandiosity or
+					smugness on my part. If you find yourself with less traffic than you
+					feel you reasonably deserve, I invite you to try the same.{' '}
+					<Link
+						className={`no-deco lowercase style-1 thin spaced color-2 rounded darken`}
+						href="/contact"
+					>
+						Let me know
+					</Link>{' '}
+					what happens.
+				</p>
 				<h3
 					className={`lowercase style-1 thin spaced left width-full block left-align`}
 				>
 					Web Traffic Limiter: User Intimidation
 				</h3>
 				<p className={`paragraph width-full block left-align`}>
-					AI and algorithms aside, we live in an anxiety society.
+					Human interaction follows a very similar pattern as those mentioned in
+					the previous section. This is not surprising. AI and algorithms intend
+					to model after human behavior, so naturally, human behavior shares
+					symmetry with AI behavior. Although the pattern is the same, the
+					reason can be quite different.
+				</p>
+				<p className={`paragraph width-full block left-align`}>
+					While true that human visitors&apos; sifting and scanning efficiency
+					is catalyzed by clear, focused, and well-formatted content - as with
+					AI - that often isn&apos;t the highest-leverage shift that we see. So
+					what is?
+				</p>
+				<p className={`paragraph width-full block left-align`}>
+					It is something that comes downstream of that - lessened user
+					intimidation. One of the most pervasive anxieties shared by people the
+					world over is the fear of looking foolish or receiving criticism from
+					someone whom one admires. Most of us admire those we perceive to be
+					talented and competent. Many today are burdened with worry, impostor
+					syndrome, and the fear of looking dumb.
+				</p>
+				<p className={`paragraph width-full block left-align`}>
+					What that means for us as writers and content creators is that when we
+					are composing a post, ad, or article, we now have a point of reference
+					against which to test our content: imagined user comfort. Some may
+					prefer to filter out inexperienced or low-intent users through tone or
+					ambiguity, but I prefer to invite the curious. That&apos;s just me. I
+					saw{' '}
+					<Link
+						className={`no-deco lowercase style-1 thin spaced color-2 rounded darken`}
+						href={`https://www.nytimes.com/2025/12/19/well/high-performers-multiple-disciplines-early-study.html#:~:text=Young%20prodigies%20%E2%80%94%20the%20teenage%20sports,journeys%20in%20very%20different%20ways.`}
+					>
+						a study
+					</Link>{' '}
+					recently, the results of which suggest that late bloomers tend to
+					become high achievers more frequently than child prodigies.
+				</p>
+				<p className={`paragraph width-full block left-align`}>
+					When I keep that in mind, it is much easier for me to imagine taking
+					on a student or a client whose idea is solid, even if some aspects are
+					rough around the edges, or their background or presentation are
+					non-standard.
+				</p>
+				<iframe
+					width="560"
+					height="315"
+					src="https://www.youtube.com/embed/Dyy3d8MnlA4?si=wCHyossmMraIKfHi"
+					title="YouTube video player"
+					frameBorder={0}
+					allowFullScreen
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				></iframe>
+				<p className={`paragraph width-full block left-align`}>
+					If you want to take a similar stance and invite as many passionately
+					curious people to your content as possible, that can be as easy as
+					carrying the intention not to intimidate people like the citizen
+					scientist featured in the video above when you write and record your
+					content.
 				</p>
 			</section>
+			<section>
+				<h3
+					className={`lowercase style-1 thin spaced left width-full block left-align`}
+				>
+					How To Go from Pitfall To Prosperity
+				</h3>
+
+				<p className={`paragraph width-full block left-align`}>
+					I offer ad and content clarification services. If you want to try your
+					hand at it yourself, take a second look at your content while holding
+					these thoughts in mind. I will write more in depth pieces on the
+					individual ideas shared here later, as they each deserve to be looked
+					at closely and broken apart. Check back later!
+				</p>
+				<p className={`paragraph width-full block left-align`}>
+					For now, in the spirit of keeping Patrick&apos;s Pitfalls short and
+					sweet, I am going to wrap this up.
+				</p>
+			</section>
+			<Footer title={"'Patrick's Pitfalls - Vague Wording'"} />
 		</main>
 	);
 }
