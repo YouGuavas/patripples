377991322c82966a22d7d659ff952175b9a8174c
YouGuavas <patrickyambrick@gmail.com>
2026-02-04 07:00:42 -0500

    Snipcart modal styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 import { UrlProviderWrapper } from './UrlProviderWrapper';
 import { ThemeProviderWrapper } from './ThemeProviderWrapper';
-//import CartButton from './components/shop/listings/CartButton';
+import CartButton from './components/shop/listings/CartButton';
 
 declare global {
 	interface Window {
@@ -31,13 +31,12 @@ export default function RootLayout({
 			<Head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link rel="preconnect" href="https://app.snipcart.com" />
+				<link rel="preconnect" href="https://cdn.snipcart.com" />
 				<link
 					href="https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Exo+2:ital,wght@0,100..900;1,100..900&family=Lora:ital,wght@0,400..700;1,400..700&family=Manrope:wght@200..800&family=Orbitron:wght@400..900&family=Patrick+Hand&display=swap"
 					rel="stylesheet"
 				/>
-
-				<link rel="preconnect" href="https://app.snipcart.com" />
-				<link rel="preconnect" href="https://cdn.snipcart.com" />
 				<link
 					rel="stylesheet"
 					href="https://cdn.snipcart.com/themes/v3.2.2/default/snipcart.css"
@@ -46,8 +45,7 @@ export default function RootLayout({
 			<body className={`${inter.className} bg-5`}>
 				<ThemeProviderWrapper>
 					<UrlProviderWrapper>
-						{/*						<CartButton />
-						 */}{' '}
+						<CartButton />
 						<UpdateTicker
 							updates={[
 								{
@@ -59,21 +57,22 @@ export default function RootLayout({
 						{
 							//<ShareButtonsGroup />
 						}{' '}
+						<div
+							hidden
+							id="snipcart"
+							data-api-key="OGU2OWMwOGMtNjM2Mi00MWEyLTlhYmUtYjA1MTA4YzY5MzNkNjM4MTI3MDA4NDE3MzEzOTgy"
+							data-config-modal-style="side"
+						></div>
 						<Nav />
 						<Tips />
 						{children}
 					</UrlProviderWrapper>
 				</ThemeProviderWrapper>
 			</body>
-			{/*<script
+			<script
 				async
 				src="https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.js"
 			></script>
-			<div
-				hidden
-				id="snipcart"
-				data-api-key="OGU2OWMwOGMtNjM2Mi00MWEyLTlhYmUtYjA1MTA4YzY5MzNkNjM4MTI3MDA4NDE3MzEzOTgy"
-			></div>*/}
 
 			<Analytics />
 		</html>
