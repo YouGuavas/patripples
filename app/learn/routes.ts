0fcd4dc2311bde7d97e1ecab890e0b645eb0f7ff
YouGuavas <patrickyambrick@gmail.com>
2026-02-11 05:03:08 -0500

    Update routes

--- app/learn/routes.ts
+++ app/learn/routes.ts
@@ -1,6 +1,6 @@
 const routes = [
 	{ name: 'Read', path: '/learn/read/', tags: ['x'] },
-	{ name: 'Notecards', path: '/learn/notecards/', tags: ['x'] },
+	//{ name: 'Notecards', path: '/learn/notecards/', tags: ['x'] },
 
 	//{ name: 'Count', path: '/learn/count/', tags: ['x'] },
 ];
