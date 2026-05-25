5a7d2f48286f948b224418dcca2b68c5cf3bdc59
patrickyambrick@gmail.com <patrickyambrick@gmail.com>
2026-05-25 18:47:31 -0400

    Update data vis

--- app/components/buttons/ConversionButtons.tsx
+++ app/components/buttons/ConversionButtons.tsx
@@ -9,6 +9,9 @@ export default function ConversionButton(props: propsType) {
 	const { hook, setHook } = useSales();
 	const { loss, setLoss } = useSales();
 	const { passerby, setPasserby } = useSales();
+	const { impulse, setImpulse } = useSales();
+	const { core, setCore } = useSales();
+	const { premium, setPremium } = useSales();
 
 	const handleClick = (name: string) => {
 		switch (name.toLowerCase()) {
@@ -20,6 +23,15 @@ export default function ConversionButton(props: propsType) {
 				break;
 			case 'passerby':
 				setPasserby(passerby + 1);
+				break;
+			case 'impulse purchase':
+				setImpulse(impulse + 1);
+				break;
+			case 'core purchase':
+				setCore(core + 1);
+				break;
+			case 'premium purchase':
+				setPremium(premium + 1);
 				break;
 			default:
 				break;
@@ -50,6 +62,39 @@ export default function ConversionButton(props: propsType) {
 				</button>
 			);
 		case 'passerby':
+			return (
+				<button
+					onClick={() => {
+						handleClick(props.name);
+					}}
+					className={`${props.type.toLowerCase() + '-button'} left-align style-1 lowercase rounded bordered px-1 flex column centered center darken`}
+				>
+					{props.name}
+				</button>
+			);
+		case 'impulse purchase':
+			return (
+				<button
+					onClick={() => {
+						handleClick(props.name);
+					}}
+					className={`${props.type.toLowerCase() + '-button'} left-align style-1 lowercase rounded bordered px-1 flex column centered center darken`}
+				>
+					{props.name}
+				</button>
+			);
+		case 'core purchase':
+			return (
+				<button
+					onClick={() => {
+						handleClick(props.name);
+					}}
+					className={`${props.type.toLowerCase() + '-button'} left-align style-1 lowercase rounded bordered px-1 flex column centered center darken`}
+				>
+					{props.name}
+				</button>
+			);
+		case 'premium purchase':
 			return (
 				<button
 					onClick={() => {
