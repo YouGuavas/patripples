18debe1e7f3528ae1abff28fa06f884b89009d5f
patrickyambrick@gmail.com <patrickyambrick@gmail.com>
2026-07-01 16:06:30 -0400

    Update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-import withSerWister from '@serwist/nextjs';
-const withSerwist = withSerWistInit({
+import withSerwistInit from '@serwist/next';
+const withSerwist = withSerwistInit({
 	swSrc: 'app/sw.ts',
 	swDest: 'public/sw.js',
 	disable: process.env.NODE_ENV === 'development',
