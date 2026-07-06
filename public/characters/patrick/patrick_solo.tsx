01c0ea299fe4fe45a55285ed1ef0460a0e729973
patrickyambrick@gmail.com <patrickyambrick@gmail.com>
2026-07-06 14:21:09 -0400

    Update image

--- public/characters/patrick/patrick_solo.tsx
+++ public/characters/patrick/patrick_solo.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { characterProps } from './types';
+import characterProps from '../types';
 const Patrick = (props: characterProps) => (
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
